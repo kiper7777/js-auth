558341809afebdf746876d6e89ac84393691a97d
Dmytro Kiperman <8boyss@ukr.net>
2023-12-11 23:31:55 +0100

    upd

--- src/class/user.js
+++ src/class/user.js
@@ -7,10 +7,15 @@ class User {
 
   static #list = []
 
+  static #count = 1
+
   constructor({ email, password, role }) {
-    this.email = email
-    this.password = password
+    this.id = User.#count++
+
+    this.email = String(email).toLowerCase()
+    this.password = String(password)
     this.role = User.#convertRole(role)
+    this.isConfirm = false
   }
 
   static #convertRole = (role) => {
@@ -30,8 +35,32 @@ class User {
   static create(data) {
     const user = new User(data)
 
+    console.log(user)
+
     this.#list.push(user)
+
+    console.log(this.#list)
+
+    return user
   }
+
+  static getByEmail(email) {
+    return (
+      this.#list.find(
+        (user) =>
+          user.email === String(email).toLowerCase(),
+      ) || null
+    )
+  }
+
+  static getById(id) {
+    return (
+      this.#list.find((user) => user.id === Number(id)) ||
+      null
+    )
+  }
+
+  static getList = () => this.#list
 }
 
 module.exports = {
--- src/route/auth.js
+++ src/route/auth.js
@@ -5,6 +5,12 @@ const router = express.Router()
 
 const { User } = require('../class/user')
 
+User.create({
+  email: 'test@mail.com',
+  password: 123,
+  role: 1,
+})
+
 // ================================================================
 
 // router.get Створює нам один ентпоїнт
@@ -18,7 +24,13 @@ router.get('/signup', function (req, res) {
     // вказуємо назву контейнера
     name: 'signup',
     // вказуємо назву компонентів
-    component: ['back-button', 'field', 'field-password'],
+    component: [
+      'back-button',
+      'field',
+      'field-password',
+      'field-checkbox',
+      'field-select',
+    ],
 
     // вказуємо назву сторінки
     title: 'Signup page',
@@ -41,6 +53,45 @@ router.get('/signup', function (req, res) {
   })
   // ↑↑ сюди вводимо JSON дані
 })
+
+router.post('/signup', function (req, res) {
+  const { email, password, role } = req.body
+
+  console.log(req.body)
+
+  if (!email || !password || !role) {
+    return res.status(400).json({
+      message: "Помилка. Обов'язкові поля відсутні",
+    })
+  }
+
+  try {
+    const user = User.getByEmail(email)
+
+    if (user) {
+      return res.status(400).json({
+        message: 'Помилка. Такий користувач вже існує',
+      })
+    }
+
+    const newUser = User.create({ email, password, role })
+
+    const session = Session.create(newUser)
+
+    Confirm.create(newUser.email)
+
+    return res.status(200).json({
+      message: 'Користувач успішно зареєстрованний',
+      session,
+    })
+  } catch (err) {
+    return res.status(400).json({
+      message: 'Помилка створення користувача',
+    })
+  }
+})
+
+// ================================================================
 
 // Підключаємо роутер до бек-енду
 module.exports = router
