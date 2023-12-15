51fdd0ead683930c34a28e20cb25e1355e75bc08
Dmytro Kiperman <8boyss@ukr.net>
2023-12-16 00:46:57 +0100

    upd

---
+++ src/container/login/index.js
@@ -0,0 +1,86 @@
+import {
+  Form,
+  REG_EXP_EMAIL,
+  REG_EXP_PASSWORD,
+} from '../../script/form'
+
+import { saveSession } from '../../script/session'
+
+class SignupForm extends Form {
+  FIELD_NAME = {
+    EMAIL: 'email',
+    PASSWORD: 'password',
+  }
+
+  FIELD_ERROR = {
+    IS_EMPTY: 'Введіть значення в поле',
+    IS_BIG: 'Дуже довге значення, приберіть зайве',
+    EMAIL: 'Введіть коректне значення e-mail адреси',
+  }
+
+  validate = (name, value) => {
+    if (String(value).length < 1) {
+      return this.FIELD_ERROR.IS_EMPTY
+    }
+
+    if (String(value).length > 20) {
+      return this.FIELD_ERROR.IS_BIG
+    }
+
+    if (name === this.FIELD_NAME.EMAIL) {
+      if (!REG_EXP_EMAIL.test(String(value))) {
+        return this.FIELD_ERROR.EMAIL
+      }
+    }
+  }
+
+  submit = async () => {
+    if (this.disabled === true) {
+      this.validateAll()
+    } else {
+      console.log(this.value)
+
+      this.setAlert('progress', 'Завантаження...')
+
+      try {
+        const res = await fetch('/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: this.convertData(),
+        })
+
+        const data = await res.json()
+
+        if (res.ok) {
+          console.log(data.session)
+          this.setAlert('success', data.message)
+          saveSession(data.session)
+          location.assign('/')
+        } else {
+          this.setAlert('error', data.message)
+        }
+      } catch (error) {
+        this.setAlert('error', error.message)
+      }
+    }
+  }
+
+  convertData = () => {
+    return JSON.stringify({
+      [this.FIELD_NAME.EMAIL]:
+        this.value[this.FIELD_NAME.EMAIL],
+      [this.FIELD_NAME.PASSWORD]:
+        this.value[this.FIELD_NAME.PASSWORD],
+    })
+  }
+}
+
+window.signupForm = new SignupForm()
+
+document.addEventListener('DOMContentLoaded', () => {
+  if (window.session) {
+    location.assign('/')
+  }
+})
